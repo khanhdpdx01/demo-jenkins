4bb93f328b9dda7d23e9a3207a482bc6ece404a4
Nguyen Tan Khanh <khanhdpdx@gmail.com>
2022-10-20 10:06:16 +0700

    Init

---
+++ src/main/java/org/cdcn/Calculation.java
@@ -0,0 +1,19 @@
+package org.cdcn;
+
+public class Calculation {
+    public static double add(double a, double b) {
+        return a + b;
+    }
+
+    public static double sub(double a, double b) {
+        return a - b;
+    }
+
+    public static double multi(double a, double b) {
+        return a * b;
+    }
+
+    public static double div(double a, double b) {
+        return a / b;
+    }
+}
